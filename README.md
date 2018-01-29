fbf8506c53a5b16a4fb0c519f262232f8cff94aa
Gord Brown <gdbzork@gmail.com>
2018-01-29 16:42:47 +0000

    Added some new test data, limited comparison to perfect matches.
    Compares exact matches in one file with other, reports when one finds
    exact matches but the other finds nothing (or a less-good alignment).

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# bam_compare
-Compare BAM files to see what alignments they found
+## Bam Comparator
+
+Do alignment packages really find all the alignments that are present in the
+genome?  This package compares two BAM files, typically from different
+aligners, to see if they found equivalent results.  Normal usage is to set
+the aligners' parameters to find all equally-good best matches, then
+compare them to see if they found the same set.  (In an ideal world they
+would, of course.)  For simplicity, this version of the package only considers
+exact matches.
--- bamCompare/src/main/java/uk/ac/cam/cruk/bamCompare/BamComparator.java
+++ bamCompare/src/main/java/uk/ac/cam/cruk/bamCompare/BamComparator.java
@@ -2,6 +2,7 @@ package uk.ac.cam.cruk.bamCompare;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.ListIterator;
 
 import htsjdk.samtools.SAMRecord;
 
@@ -11,21 +12,40 @@ import htsjdk.samtools.SAMRecord;
  */
 public class BamComparator {
 
-  protected boolean compareGroups(List<SAMRecord> rec1, List<SAMRecord> rec2) {
+  protected boolean compareGroups(List<SAMRecord> rec1, String rec1name, List<SAMRecord> rec2, String rec2name) {
     boolean okay = true;
 
-    String rec1name = rec1.size() > 0 ? rec1.get(0).getReadName() : null;
-    String rec2name = rec2.size() > 0 ? rec2.get(0).getReadName() : null;
-
-    if (!(rec1name.equals(rec2name))) {
-      okay = false;
-      System.out.println("Name Mismatch: " + rec1name + " != " + rec2name);
-    }
     if (rec1.size() != rec2.size()) {
       okay = false;
       System.out.println("Size Mismatch: " + rec1name + "=" + rec1.size() + " -- " + rec2name + "=" + rec2.size());
     }
     return okay;
+  }
+
+  public void report(List<SAMRecord> recs,String fn) {
+    System.out.println(fn+"\t"+recs.size()+"\t"+recs.get(0).getReadName());
+//    for (SAMRecord rec : recs) {
+//      System.out.println("U: " + rec.getContig() + ":" + rec.getAlignmentStart() + "-" + rec.getAlignmentEnd() + " : "
+//          + rec.getReadName());
+//    }
+  }
+
+  public int filterGroup(List<SAMRecord> recs) {
+    ListIterator<SAMRecord> recIt = recs.listIterator();
+    int removed = 0;
+    while (recIt.hasNext()) {
+      SAMRecord rec = recIt.next();
+      boolean remove = rec.getReadUnmappedFlag() || rec.getIntegerAttribute("NM") > 0 || rec.getCigarLength() > 1;
+//        remove = rec.getReadUnmappedFlag() || rec.getIntegerAttribute("NM") > 0;
+//      } catch (NullPointerException npe) {
+//        System.err.println("NPE: " + npe.toString());
+//      }
+      if (remove) {
+        recIt.remove();
+        removed++;
+      }
+    }
+    return removed;
   }
 
   public int compareBams(String fn1, String fn2) {
@@ -34,40 +54,82 @@ public class BamComparator {
     BamFile bf2 = new BamFile(fn2);
     List<SAMRecord> rec1;
     List<SAMRecord> rec2;
+    boolean lastRec1 = false;
+    boolean lastRec2 = false;
+    String name1 = null;
+    String name2 = null;
+    int unique1 = 0;
+    int unique2 = 0;
 
     bf1.open();
     bf2.open();
     rec1 = bf1.nextNameGroup();
+    lastRec1 = rec1.size() == 0;
+    name1 = lastRec1 ? null : rec1.get(0).getReadName();
     rec2 = bf2.nextNameGroup();
+    lastRec2 = rec2.size() == 0;
+    name2 = lastRec2 ? null : rec2.get(0).getReadName();
+    filterGroup(rec1);
+    filterGroup(rec2);
 
     while (true) {
-      String n1 = rec1.size() > 0 ? rec1.get(0).getReadName() : null;
-      String n2 = rec2.size() > 0 ? rec2.get(0).getReadName() : null;
-      if (n1 == null) {
-        if (n2 == null) {
+      if (lastRec1) {
+        if (lastRec2) {
           // end of both files
           break;
         } else {
-          System.out.println("Trailing reads in " + fn2 + ": " + n2);
+          System.out.println("Trailing reads in " + fn2 + ": " + name2);
           rec2 = bf2.nextNameGroup();
+          lastRec2 = rec2.size() == 0;
+          name2 = lastRec2 ? null : rec2.get(0).getReadName();
+          filterGroup(rec2);
+          discrepancies++;
         }
-      } else if (n2 == null) {
-        System.out.println("Trailing reads in " + fn1 + ": " + n1);
+      } else if (lastRec2) {
+        System.out.println("Trailing reads in " + fn1 + ": " + name1);
         rec1 = bf1.nextNameGroup();
+        lastRec1 = rec1.size() == 0;
+        name1 = lastRec1 ? null : rec1.get(0).getReadName();
+        filterGroup(rec1);
+        discrepancies++;
       } else {
-        int comp = n1.compareTo(n2);
+        int comp = name1.compareTo(name2);
         if (comp == 0) { // names match
-          if (!compareGroups(rec1, rec2)) {
+          if (rec1.size() == 0 && rec2.size() == 0) { // no exact matches (or unaligned)
+            // nothing to do
+          } else if (rec1.size() == 0) {
+            report(rec2,fn2);
+            discrepancies++;
+            unique2++;
+          } else if (rec2.size() == 0) {
+            report(rec1,fn1);
+            discrepancies++;
+            unique1++;
+          } else if (!compareGroups(rec1, name1, rec2, name2)) {
             discrepancies++;
           }
           rec1 = bf1.nextNameGroup();
+          lastRec1 = rec1.size() == 0;
+          name1 = lastRec1 ? null : rec1.get(0).getReadName();
           rec2 = bf2.nextNameGroup();
+          lastRec2 = rec2.size() == 0;
+          name2 = lastRec2 ? null : rec2.get(0).getReadName();
+          filterGroup(rec1);
+          filterGroup(rec2);
         } else if (comp < 0) { // name missing in bf2
-          System.out.println("Extra Read in " + fn1 + ": " + n1);
+          System.out.println("Extra Read in " + fn1 + ": " + name1);
           rec1 = bf1.nextNameGroup();
+          lastRec1 = rec1.size() == 0;
+          name1 = lastRec1 ? null : rec1.get(0).getReadName();
+          filterGroup(rec1);
+          discrepancies++;
         } else {
-          System.out.println("Extra Read in " + fn2 + ": " + n2);
+          System.out.println("Extra Read in " + fn2 + ": " + name2);
           rec2 = bf2.nextNameGroup();
+          lastRec2 = rec2.size() == 0;
+          name2 = lastRec2 ? null : rec2.get(0).getReadName();
+          filterGroup(rec2);
+          discrepancies++;
         }
       }
     }
@@ -77,6 +139,8 @@ public class BamComparator {
     } catch (IOException ioe) {
       System.err.println("Got error closing BAM files: " + ioe.toString());
     }
+    System.out.println(fn1+": "+unique1+" unique");
+    System.out.println(fn2+": "+unique2+" unique");
     return discrepancies;
   }
 
